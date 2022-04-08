a3d393b5c04864e3cbe414a16c8e9840b22552c4
vinezi 1 <wewewe.ww52@gmail.com>
2022-04-08 21:26:32 +0300

    bugfix

--- mo8_1/MainWindow.xaml.cs
+++ mo8_1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading;
 using System.Windows;
@@ -12,9 +13,10 @@ namespace mo8_1
 {
     public partial class MainWindow : Window
     {
-        private static int maximumPermissibleWeight, numberOfItems, numberOfIndividuals, probabilityOfMutation, numberOfTheBest, percentageOfQuality, numberOfRuns;
+        BackgroundWorker bw;
+        private static int maximumPermissibleWeight, numberOfItems, numberOfIndividuals, probabilityOfMutation, numberOfTheBest, percentageOfQuality, numberOfRuns, 
+            allItemPrice, allItemWeight;
         
-
         ObservableCollection<Item> rowDataItems = new ObservableCollection<Item>();
         ObservableCollection<Item> rowDataSelectedItems = new ObservableCollection<Item>();
 
@@ -28,40 +30,29 @@ namespace mo8_1
         #region GeneticAlgoritmFunction  
         private int getQuality(int weight, int price)
         {
-    
             int result;
-            //if (weight <= Convert.ToInt32(valueMax.Text))
             if (weight <= maximumPermissibleWeight)
                 result = 100;
             else
                 result = maximumPermissibleWeight - weight;
-                //result = Convert.ToInt32(valueMax.Text) - weight;
-            result += price * 100 / Convert.ToInt32(priceInfo.Text);
+            result += price * 100 / allItemPrice;
 
             if (result < 0)
-            {
                 result = 0;
-            }
             else
-            {
                 result /= 2;
-            }
-
             return result;
         }
-
         private void mutation()
         {
             Random rand = new Random();
-            int num = Convert.ToInt32(valueNum.Text), mutationPercent = Convert.ToInt32(valueMutation.Text);
             int editNum;
             string patern;
-
             foreach (Chromosome item in rowPopulation)
             {
-                if (rand.Next(101) <= mutationPercent)
+                if (rand.Next(101) <= probabilityOfMutation)
                 {
-                    editNum = rand.Next(0, num);
+                    editNum = rand.Next(0, numberOfItems);
                     string right = item.chromosome.Substring(editNum + 1);
                     patern = item.chromosome[editNum] == '0' ? "1" : "0";
                     item.chromosome = item.chromosome.Remove(editNum) + patern + right;
@@ -69,17 +60,6 @@ namespace mo8_1
             }
             updateData();
         }
-        //private string crossing(string parent1, string parent2) ///TO DO rebild function to automatic
-        //{
-        //    int cut = parent1.Length / 2;
-        //    string parent1Right = parent1.Substring(cut);
-        //    string parent2Right = parent2.Substring(cut);
-
-        //    parent1 = parent1.Remove(cut) + parent2Right;
-        //    parent2 = parent2.Remove(cut) + parent1Right;
-        //    return parent1 + " " + parent2;
-        //}
-
         private void crossing()
         {
             string parent1, parent2;
@@ -107,7 +87,6 @@ namespace mo8_1
         #region Events
         private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            
             if (e.Key == Key.F4)
             {
                 status.Background = new SolidColorBrush(Color.FromRgb(90, 33, 204));
@@ -119,54 +98,23 @@ namespace mo8_1
                 valueMutation.Text = "5";
                 valueStopCycle.Text = "10";
                 valueStopPercent.Text = "90";
-                
+                generateThings();
+                generatePopulation();
+                Calculate();
             }
             if (e.Key == Key.F9)
             {
-                Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new ThreadStart(delegate
-                {
-                    TestFunc();
-                    Thread.Sleep(200);
-                }));
-
-
-
-
-               
-                /*
-                //valueMax.Text = "50";
-                //valueNum.Text = "30";
-                //valuePopulation.Text = "2000";
-                //valueBest.Text = "1";
-                //valueMutation.Text = "5";
-                //valueStopCycle.Text = "5000";
-                //valueStopPercent.Text = "70";
-                //generateThings();
-                //generatePopulation();
-                //Calculate();*/
+                valueMax.Text = "30";
+                valueNum.Text = "50";
+                valuePopulation.Text = "2000";
+                valueBest.Text = "1";
+                valueMutation.Text = "5";
+                valueStopCycle.Text = "5000";
+                valueStopPercent.Text = "70";
+                generateThings();
+                generatePopulation();
+                Calculate();
             }
-            //Application.Current.Dispatcher.Invoke(new Action(() =>
-            //{
-            //    status.Background = new SolidColorBrush(Color.FromRgb(0, 122, 204));
-            //    textStatus.Text = "Ready";
-            //}));
-
-            generateThings();
-            generatePopulation();
-            Calculate();
-        }
-
-        private void TestFunc()
-        {
-            status.Background = new SolidColorBrush(Color.FromRgb(90, 33, 204));
-            textStatus.Text = "Running";
-            valueMax.Text = "50";
-            valueNum.Text = "30";
-            valuePopulation.Text = "2000";
-            valueBest.Text = "1";
-            valueMutation.Text = "5";
-            valueStopCycle.Text = "5000";
-            valueStopPercent.Text = "70";
         }
         private void generateItems_Click(object sender, RoutedEventArgs e)
         {
@@ -206,53 +154,86 @@ namespace mo8_1
         {
             if (chekingInput("start"))
             {
-                //Application.Current.Dispatcher.Invoke(new Action(() =>
-                //{
-                //    status.Background = new SolidColorBrush(Color.FromRgb(90, 33, 204));
-                //    textStatus.Text = "Running";
-                //}));
+                bw = new BackgroundWorker();
 
-                crossing();
-                mutation();
-                saveBest();
-                int stopPercent = Convert.ToInt32(valueStopPercent.Text);
-                int stopCycle = Convert.ToInt32(valueStopCycle.Text);
-                for (int i = 0; i < stopCycle; i++)
+                // this allows our worker to report progress during work
+                // это позволяет нашему воркеру сообщать о прогрессе во время работы
+                bw.WorkerReportsProgress = true;
+
+                // what to do in the background thread
+                // что делать в фоновом потоке
+                bw.DoWork += new DoWorkEventHandler(
+                delegate (object o, DoWorkEventArgs args)
                 {
+                    BackgroundWorker b = o as BackgroundWorker;
+
                     crossing();
                     mutation();
-                    if (compareWithTheBest())
+                    saveBest();
+                    for (int i = 0; i < numberOfRuns; i++)
                     {
-                        saveBest();
+                        b.ReportProgress(i * 100 / (numberOfRuns - 1));
+                        crossing();
+                        mutation();
+                        if (compareWithTheBest())
+                        {
+                            saveBest();
+                        }
+                        if (rowsTheBestPopulation.ToList()[0].percent >= percentageOfQuality)
+                        {
+                            MessageBox.Show("Завершение по достижению требуемого процента");
+                            break;
+                        }
+                        if (i == numberOfRuns - 1)
+                        {
+                            MessageBox.Show("Завершение по количеству выполнений");
+                        }
+
+                        //Application.Current.Dispatcher.Invoke(new Action(() =>
+                        //{
+                        //    status.Background = new SolidColorBrush(Color.FromRgb(0, 122, 204));
+                        //    textStatus.Text = "Ready";
+                        //}));
                     }
-                    if (rowsTheBestPopulation.ToList()[0].percent >= stopPercent)
-                    {
-                        MessageBox.Show("Завершение по достижению требуемого процента");
-                        break;
-                    }
-                    if (i == stopCycle - 1)
-                    {
-                        MessageBox.Show("Завершение по количеству выполнений");
-                    }
-                }
-                //Application.Current.Dispatcher.Invoke(new Action(() =>
-                //{
-                //    status.Background = new SolidColorBrush(Color.FromRgb(0, 122, 204));
-                //    textStatus.Text = "Ready";
-                //}));
+                    RefreshDatagrids();
+
+
+
+                });
+                bw.ProgressChanged += new ProgressChangedEventHandler(
+                delegate (object o, ProgressChangedEventArgs args)
+                {
+                    textStatus.Text = string.Format("{0}% Completed", args.ProgressPercentage);
+                    //status.Background = new SolidColorBrush(Color.FromRgb(255, 122, 204));
+                    status.Background = new SolidColorBrush(Color.FromRgb(202, 81, 0));
+
+                    //RefreshDatagrids();
+                });
+                bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(
+                delegate (object o, RunWorkerCompletedEventArgs args)
+                {
+                    status.Background = new SolidColorBrush(Color.FromRgb(0, 122, 204));
+                    textStatus.Text = "Finished!";
+                });
+                
+                bw.RunWorkerAsync();
             }
+            else
+                MessageBox.Show("biba");           
         }
+
+
+
         private void generateThings()
         {
             if (chekingInput("backpack"))
             {
                 rowDataItems.Clear();
-                int NumOfItems = int.Parse(valueNum.Text);
                 int priceTmp, weightTmp;
                 int priceTotal = 0, weightTotal = 0;
 
                 Random rnd = new Random();
-                for (int i = 0; i < NumOfItems; i++)
+                for (int i = 0; i < numberOfItems; i++)
                 {
                     priceTmp = rnd.Next(1, 15);
                     weightTmp = rnd.Next(1, 20);
@@ -261,6 +242,8 @@ namespace mo8_1
                     priceTotal += priceTmp;
                     weightTotal += weightTmp;
                 }
+                allItemPrice = priceTotal;
+                allItemWeight = weightTotal;
                 priceInfo.Text = priceTotal.ToString();
                 weightInfo.Text = weightTotal.ToString();
                 dataGRBackpack.ItemsSource = rowDataItems;
@@ -271,18 +254,12 @@ namespace mo8_1
             if(chekingInput("population")){
                 rowPopulation.Clear();
                 Random rnd = new Random();
-                long rndArg = (long)Math.Pow(2, Convert.ToInt32(valueNum.Text));
+                long rndArg = (long)Math.Pow(2, numberOfItems), tmp;
                 string chromosome;
-                long tmp;
-                int weightTmp, priceTmp, populationCount = Convert.ToInt32(valuePopulation.Text);
-                for (int i = 0; i < populationCount; i++)
+                for (int i = 0; i < numberOfIndividuals; i++)
                 {
                     tmp = (long)(rnd.NextDouble() * rndArg);
                     chromosome = Convert.ToString(tmp, 2);
-
-                    weightTmp = calculateWeight(chromosome);
-                    priceTmp = calculatePrice(chromosome);
-
                     rowPopulation.Add(new Chromosome { name = "name " + rnd.Next(5000), chromosome = addNonSignificantZero(chromosome) });
                 }
                 dataGPopulation.ItemsSource = rowPopulation;
@@ -303,19 +280,20 @@ namespace mo8_1
                 item.price = priceTmp;
                 item.percent = getQuality(weightTmp, priceTmp);
             }
-            dataGPopulation.Items.Refresh();
+
+            //dataGPopulation.Items.Refresh();
         }
         private void saveBest()
         {
-            rowsTheBestPopulation.Clear();
+            //rowsTheBestPopulation.Clear();
+            rowsTheBestPopulation = new ObservableCollection<Chromosome>();
             List<Chromosome> SortedList = rowPopulation.ToList().OrderByDescending(o => o.percent).ToList();
-            int bectNum = Convert.ToInt32(valueBest.Text);
-            for (int i = 0; i < bectNum; i++)
+            for (int i = 0; i < numberOfTheBest; i++)
             {
                 rowsTheBestPopulation.Add(SortedList[i]);
             }
-            dataGTheBestInPopulation.ItemsSource = rowsTheBestPopulation;
-            dataGTheBestInPopulation.Items.Refresh();
+            //dataGTheBestInPopulation.ItemsSource = rowsTheBestPopulation;
+            //dataGTheBestInPopulation.Items.Refresh();
         }
         private bool compareWithTheBest()
         {
@@ -361,10 +339,9 @@ namespace mo8_1
         private string addNonSignificantZero(string chromosome)
         {
             string completeChromosome = chromosome;
-            int itemsCount = Convert.ToInt32(valueNum.Text);
-            if (chromosome.Length < itemsCount)
+            if (chromosome.Length < numberOfItems)
             {
-                for (int i = 0; i < itemsCount - chromosome.Length; i++)
+                for (int i = 0; i < numberOfItems - chromosome.Length; i++)
                 {
                     completeChromosome = "0" + completeChromosome;
                 }
@@ -381,7 +358,6 @@ namespace mo8_1
                         result = false;
                     break;
                 case "population":
-                    //int.TryParse(valuePopulation.Text, out numberOfIndividuals) || int.TryParse(valueBest.Text, out numberOfTheBest) || int.TryParse(valueMutation.Text, out probabilityOfMutation) || 
                     if (!int.TryParse(valuePopulation.Text, out numberOfIndividuals) || !int.TryParse(valueBest.Text, out numberOfTheBest)
                         || !int.TryParse(valueMutation.Text, out probabilityOfMutation) || rowDataItems.Count == 0)
                         result = false;
@@ -407,16 +383,17 @@ namespace mo8_1
             }
             return result;
         }
-
-        //private bool tryGetter()
-        //{
-        //    int.TryParse(valueNum.Text, out numberOfItems);
-        //    int.TryParse(valueMax.Text, out maximumPermissibleWeight);
-        //    return int.TryParse(valueNum.Text, out numberOfItems) || int.TryParse(valueMax.Text, out maximumPermissibleWeight) //||
-        //        int.TryParse(valuePopulation.Text, out numberOfIndividuals) || int.TryParse(valueBest.Text, out numberOfTheBest) || int.TryParse(valueMutation.Text, out probabilityOfMutation) || 
-        //       int.TryParse(valueStopPercent.Text, out percentageOfQuality) || int.TryParse(valueStopCycle.Text, out numberOfRuns)
-        //       ;
-        //}
         #endregion
+
+
+        private void RefreshDatagrids()
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                dataGPopulation.Items.Refresh();
+                dataGTheBestInPopulation.ItemsSource = rowsTheBestPopulation;
+                dataGTheBestInPopulation.Items.Refresh();
+            }));
+        }
     }
 }
